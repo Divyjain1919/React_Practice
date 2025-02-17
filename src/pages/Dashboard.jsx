381b35ad44181cb5bf8596e8520c8974daa84da3
Divyjain1919 <divyanshjain0986@gmail.com>
2025-02-17 12:33:53 +0530

    CRUD Operations

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    
+    <div>Dashboard
+      <form>
+        <input type="text" placeholder="username" />
+        <br/>
+        <input type="text" placeholder="email" />
+        <br/>
+        <input type="text" placeholder="password" />
+        <br/>
+        <div>
+          <label>Gender:</label>
+          <input type="radio" name="gender" value="male" /> Male
+          <input type="radio" name="gender" value="female" /> Female
+         
+        </div>
+        <br/>
+        <textarea type ="text " placeholder='address' />
+        <br/>
+        <label>Country</label>
+        <select>
+          <option value="india">india</option>
+          <option value="usa">usa</option>
+          <option value="uk">uk</option>
+          </select>
+          <br/>
+          <label>File</label>
+          <input type ="file"/>
+          <br/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
---
+++ src/pages/MyAccount.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyAccount = () => {
+  return (
+    <div style={{ width: "50%", height: "50vh", background: "beige" }}>
+      MyAccount
+    </div>
+  );
+};
+
+export default MyAccount;
